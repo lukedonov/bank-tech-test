84449f57c50da55c5597337dd2b237bff4c8f6f6
Luke Donovan <lukelamnisos@icloud.com>
2020-04-20 17:07:53 +0100

    refactored and tidied up code

--- src/bank.js
+++ src/bank.js
@@ -1,44 +1,26 @@
 function Bank(){
-  this.balance = 0
-  this.transactions = []
+  this.balance = 0;
+  this.transactions = [];
 }
 
 Bank.prototype.deposit = function(amount) {
-  var deposit = ["deposit",amount, this._getTodaysDate(), (this.balance + amount)]
-  this.transactions.push(deposit)
-  this.balance += amount
-}
+  var deposit = ["deposit",amount, this._getTodaysDate(), (this.balance + amount)];
+  this.transactions.push(deposit);
+  this.balance += amount;
+};
 
 Bank.prototype.withdraw = function(amount) {
   if(amount > this.balance) {
-    throw new Error("Insufficient funds")
+    throw new Error("Insufficient funds");
   } else {
-    var withdrawal = ["withdraw",amount, this._getTodaysDate(), (this.balance - amount)]
-    this.transactions.push(withdrawal)
-    this.balance -= amount
+    var withdrawal = ["withdraw",amount, this._getTodaysDate(), this.balance - amount];
+    this.transactions.push(withdrawal);
+    this.balance -= amount;
   }
-}
-
-Bank.prototype.returnBankStatement = function() {
-  statement = "date || credit || debit || balance"
-  this.transactions.reverse()
-
-  for(i = 0; i < this.transactions.length; i++) {
-    var transaction = this.transactions[i]
-
-    if(transaction[0] === "deposit"){
-      statement += `\n${transaction[2]} || ${transaction[1]} || || ${transaction[3]}`
-    }
-
-    if(transaction[0] === "withdraw") {
-      statement += `\n${transaction[2]} || || ${transaction[1]} || ${transaction[3]}`
-    }
-  }
-  return statement
-}
+};
 
 Bank.prototype._getTodaysDate = function() {
-  today = new Date()
+  var today = new Date();
   var dd = today.getDate();
   var mm = today.getMonth()+1; 
   var yyyy = today.getFullYear();
@@ -51,6 +33,22 @@ Bank.prototype._getTodaysDate = function() {
     mm='0'+mm;
   } 
   return dd+'/'+mm+'/'+yyyy;
-}
+};
 
-bank = new Bank()
+Bank.prototype.returnBankStatement = function() {
+  var statement = "date || credit || debit || balance";
+  this.transactions.reverse();
+
+  for(var i = 0; i < this.transactions.length; i++) {
+    var transaction = this.transactions[i];
+
+    if(transaction[0] === "deposit"){
+      statement += `\n${transaction[2]} || ${transaction[1]} || || ${transaction[3]}`;
+    }
+
+    if(transaction[0] === "withdraw") {
+      statement += `\n${transaction[2]} || || ${transaction[1]} || ${transaction[3]}`;
+    }
+  }
+  return statement;
+};
