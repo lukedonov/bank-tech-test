5fdc8b503cc2551ce7f24669d07707a047fd2e46
Luke Donovan <lukelamnisos@icloud.com>
2020-04-21 18:17:18 +0100

    created spy object for transaction class

--- spec/transactionModelSpec.js
+++ spec/transactionModelSpec.js
@@ -1,15 +1,22 @@
 describe("Transaction", function() {
   var transaction;
 
-  // beforeEach(function() {
-  //   transaction = new Transaction(amount);
-  // });
+  beforeEach(function() {
+    transaction = jasmine.createSpyObj('transaction',['amount','date','type']);
+    transaction.amount = 500
+    transaction.type = "deposit"
+    transaction.date = "25/12/2019"
+  });
 
-  // it("has a default amount value of 0", function() {
-  //   expect(transaction.amount).toExist();
-  // })
+  it("has a default amount value of 0", function() {
+    expect(transaction.amount).toEqual(500);
+  })
 
-  // it("has a date as an empty string as default", function() {
-  //   expect(transaction.date).toEqual("")
-  // })
+  it("has a date as an empty string as default", function() {
+    expect(transaction.date).toEqual("25/12/2019")
+  })
+
+  it("has a date as an empty string as default", function() {
+    expect(transaction.type).toEqual("deposit")
+  })
 });
